65f139d2c12d16defd8fa10d109bb0101ad37d4e
kmcginley <kmcginley@invaio.com>
2020-03-29 16:17:28 -0400

    first commit

---
+++ Vue/inventory/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title size="large" text-center>Inventory Check-in</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <inventoryEntry v-on:get-codes="getInventory" />
+    </ion-content>
+  </div>
+</template>
+
+<script>
+  import inventoryEntry from "../components/inventoryEntry";
+  export default {
+    name: "Home",
+    components: { inventoryEntry },
+    methods: {
+      getInventory(inv) {
+        console.log(inv);
+      }
+    }
+  };
+</script>
